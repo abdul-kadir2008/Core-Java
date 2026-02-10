596d5f675228ddd380d333a532420e100e093065
user <user@DESKTOP-AUIPGLK.bbrouter>
2026-02-10 17:48:41 +0530

    Pattern Question

---
+++ CoreJava/src/com/jru/Pattern/MultiplicationTablePattern.java
@@ -0,0 +1,20 @@
+package com.jru.Pattern;
+
+import java.util.Iterator;
+
+public class MultiplicationTablePattern {
+	void main() {
+	int row=0;
+	 int column =0;
+	  for (int i= 1;i<=5;i++) {
+		  
+		  for (int j=1;j<=5;j++) {
+			  System.out.print(i*j);  
+		  }
+		  System.out.println();
+		  
+	  }
+	
+}
+	
+}
--- CoreJava/src/com/jru/StringHandling/ReverseOrder.java
+++ CoreJava/src/com/jru/StringHandling/ReverseOrder.java
@@ -1,7 +1,21 @@
 package com.jru.StringHandling;
 
+class BankSystem {
+private double blance =200;
+public void checkblance() {
+	System.out.println(this.blance);
+}
+
+}
 public class ReverseOrder {
 	public static void main(String[] args) {
-		System.out.println("HElllo");
+BankSystem custamer = new BankSystem();
+
+System.out.println("-----------Welcome To Bank Of India--------------");
+System.out.println("1) Check Blance..");
+System.out.println("2) Deposite Amount..");
+System.out.println("3) Withdrar Amount..");
+int choice = Integer.parseInt(IO.readln("Chouse the Option According to requement .."));
+
 	}
 }
