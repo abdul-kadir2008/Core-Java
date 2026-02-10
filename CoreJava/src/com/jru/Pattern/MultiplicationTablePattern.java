fc5a024d70b0c97d75ff49946a2a3f3d09f3aebb
user <ka2355504@gmail.com>
2026-02-10 18:06:09 +0530

    Update in Loop

--- CoreJava/src/com/jru/Pattern/MultiplicationTablePattern.java
+++ CoreJava/src/com/jru/Pattern/MultiplicationTablePattern.java
@@ -4,8 +4,7 @@ import java.util.Iterator;
 
 public class MultiplicationTablePattern {
 	void main() {
-	int row=0;
-	 int column =0;
+	
 	  for (int i= 1;i<=5;i++) {
 		  
 		  for (int j=1;j<=5;j++) {
